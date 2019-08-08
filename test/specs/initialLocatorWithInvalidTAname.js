bccd865bc1b9752158c2633a1c4b57ca170e42e3
Igor Shapovalov <shapovalovii2000@gmail.com>
2019-08-08 14:36:54 +0300

    updated error message for invalid TA name

--- test/specs/initialLocatorWithInvalidTAname.js
+++ test/specs/initialLocatorWithInvalidTAname.js
@@ -9,7 +9,7 @@ const { driver, checkErrorMessage } = require('../utils/driver');
         await myDriver.manage().setTimeouts({implicit: 5000});
         await myDriver.get('https://trueautomation.io');
 
-        let Error = 'TrueAutomation locator ta-Name contains unsupported characters. Please make sure to use only letters, numbers, colon and underscore symbols in locator names.';
+        let Error = 'Such TrueAutomation locator name ta-Name can not be used. Locator name may consist of several parts separated by a colon. English letters, numbers and single underscores that are not at the beginning or at the end may be used.';
         let withTALocator = By.xpath(ta('ta-Name', '//span[@class=\'RveJvd snByac\']'));
         checkErrorMessage(myDriver, withTALocator, Error);
 
